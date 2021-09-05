36d8f5a80c034adf9fac77b36c608c3109dc8929
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2021-09-05 20:00:39 +0700

    [Edit] index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import ImageSlide from './ImageSlide';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ImageSlide from "./ImageSlide";
 
 export { ImageSlide }
-
